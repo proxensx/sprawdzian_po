c653de795411d850c241e9494931cd4bf47ae67b
Krzysztof Bagan <pitipl1212@gmail.com>
2023-11-30 20:24:54 +0100

    2

--- app.py
+++ app.py
@@ -7,7 +7,38 @@ users = [
     {"id": 1, "name": "Wojciech", "lastname": "Oczkowski"}
 ]
 
+
 @app.get('/users')
 def get_all_users():
-    return jsonify({'users': users})
+    return jsonify(users), 200
 
+
+@app.get('/users/<int:user_id>')
+def get_user(user_id):
+    user = next((user for user in users if user['id'] == user_id), None)
+    if user:
+        return jsonify(user), 200
+    return jsonify({'error': 'User not found'}), 404
+
+
+@app.post('/users')
+def create_user():
+    pass
+
+@app.patch('/users/<id>')
+def update_user(id):
+    pass
+
+
+@app.put('/users/<int:user_id>')
+def replace_user(id):
+    pass
+
+
+@app.delete('/users/<int:user_id>')
+def delete_user(id):
+    pass
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
